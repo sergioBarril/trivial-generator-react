af748e43d491cd7ed21bf762f887068eb7c7b664
Sergio Barril <tropped@gmail.com>
2024-11-26 00:43:40 +0100

    Trivial generator with EJS

--- src/main/trivial-generator/template-render.ts
+++ src/main/trivial-generator/template-render.ts
@@ -1,19 +1,31 @@
 import ejs from "ejs";
 import fs from "fs";
 
-import counter from "./counter.ejs?asset";
+import templatePath from "./trivial.template.ejs?asset";
 
 export async function renderTemplate() {
   // Data to pass to the template
+
+  const songs = [
+    {
+      id: "aSvlULw3zHI",
+      anime: "Patata",
+      difficulty: "normal",
+      band: "Del patio",
+      name: "Sergio",
+      oped: "Ending"
+    }
+  ];
+
   const data = {
-    count: 0 // Initial count value
+    songs
   };
 
   // Render the template with the data
-  const renderedHtml = await ejs.renderFile(counter, data);
+  const renderedHtml = await ejs.renderFile(templatePath, data);
 
   // Save the rendered HTML to a file (optional)
-  const outputPath = "./counter.html";
+  const outputPath = "./trivial.html";
   fs.writeFileSync(outputPath, renderedHtml);
 
   // Log success message
